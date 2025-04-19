f4744d60ab40c387a83b9032dfbbac0f6de31f28
Your Name <you@example.com>
2025-04-19 01:08:47 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -124,7 +124,7 @@ Pair * nextMap(HashMap * map) {
     if (map == NULL || map->buckets == NULL || map->size == 0 || map->current == -1) return NULL;
     
     long posOri = map->current;
-    for (long i = 1; i < map->capacity; i++){
+    for (long i = 0; i < map->capacity; i++){
         long newPos = (posOri + i) % map->capacity;
 
         if (map->buckets[newPos] != NULL && map->buckets[newPos]->key != NULL){
