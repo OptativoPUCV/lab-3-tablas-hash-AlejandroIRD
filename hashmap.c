b57aa322ca5bafe41cd2d65d16e6533b2ddd3a28
Your Name <you@example.com>
2025-04-18 23:50:54 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -55,7 +55,7 @@ HashMap * createMap(long capacity) {
     HashMap * map = (HashMap*)malloc(sizeof(HashMap));
     if (map == NULL) return NULL;
     
-    map->buckets = (Pair**)malloc(sizeof(pair*)* capacity);
+    map->buckets = (Pair**)malloc(sizeof(Pair*)* capacity);
     if (map->buckets == NULL){
         free(map);
         return NULL;
