468510fc5f0710a0ea83ccf239edf150aebccc38
Your Name <you@example.com>
2025-04-19 00:37:18 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,7 +122,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-    if (map == NULL || map->buckets == NULL || map->size == 0 || map->current) return NULL;
+    if (map == NULL || map->buckets == NULL || map->size == 0 || map->current == NULL) return NULL;
     
     long posOri = map->current;
     for (long i = 1; i < map->capacity; i++){
